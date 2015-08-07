be0c0d89b49dedacb377af8a305b1f9d41e375d5
Adrian Mejia <adriansky@gmail.com>
2015-08-07 07:16:58 -0400

    create products factory and shows products mock data

---
+++ client/app/products/products.service.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('meanshopApp')
+  .factory('Product', function () {
+    return [
+      {id: 1, title: 'Product 1', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'},
+      {id: 2, title: 'Product 2', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'},
+      {id: 3, title: 'Product 3', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'},
+      {id: 4, title: 'Product 4', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'},
+      {id: 5, title: 'Product 5', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'}
+    ];
+  });
