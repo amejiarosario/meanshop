dc8e8147b72fdafc4a0247759496984269c65031
Adrian Mejia <adriansky@gmail.com>
2015-08-22 18:06:13 -0400

    styling and branding
    fix CRFL line endings by denoting images as binaries in .gitattributes

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,27 +1,6 @@
 'use strict';
 
 angular.module('meanshopApp')
-  .controller('MainCtrl', function($scope, $http, socket) {
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
-
-    $scope.addThing = function() {
-      if ($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
-    });
+  .controller('MainCtrl', function($scope, $http, socket, Product) {
+    $scope.products = Product.query().slice(3);
   });
