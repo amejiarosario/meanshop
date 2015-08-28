679136549ce734c69b215c0b920efd81ef10279f
Adrian Mejia <adriansky@gmail.com>
2015-08-27 22:15:17 -0400

    [generator enhancement] shows IP address in the log

--- server/app.js
+++ server/app.js
@@ -35,7 +35,7 @@ require('./routes')(app);
 // Start server
 function startServer() {
   server.listen(config.port, config.ip, function() {
-    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+    console.log('Express server listening on %s:%d, in %s mode', config.ip, config.port, app.get('env'));
   });
 }
 
