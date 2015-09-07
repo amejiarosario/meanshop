bff220c4c951abb5aebde44ea6d95574c778fb19
Adrian Mejia <adriansky@gmail.com>
2015-09-07 17:56:38 -0400

    Implementing a RESTful Product Service

--- client/app/products/products.service.js
+++ client/app/products/products.service.js
@@ -1,56 +1,9 @@
 'use strict';
 
 angular.module('meanshopApp')
-  .factory('Product', function () {
-    var lastId = 5;
-    var exampleProducts = [
-      {_id: 1, title: 'Product 1', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'},
-      {_id: 2, title: 'Product 2', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'},
-      {_id: 3, title: 'Product 3', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'},
-      {_id: 4, title: 'Product 4', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'},
-      {_id: 5, title: 'Product 5', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'}
-    ];
-
-    return {
-      query: function(){
-        return exampleProducts;
-      },
-
-      get: function(params){
-        var result = {};
-        angular.forEach(exampleProducts, function (product) {
-          if(product._id === parseInt(params.id)){
-            this.product = product;
-            return;
-          }
-        }, result);
-        return result.product;
-      },
-
-      delete: function(params){
-        angular.forEach(exampleProducts, function (product, index) {
-          if(product._id === params._id){
-            console.log(product, index);
-            exampleProducts.splice(index, 1);
-            return;
-          }
-        });
-      },
-
-      create: function(product){
-        product._id = ++lastId;
-        exampleProducts.push(product);
-      },
-
-      update: function(product){
-        var item = this.get(product);
-        if(!item) { return false; }
-
-        item.title = product.title;
-        item.price = product.price;
-        item.quantity = product.quantity;
-        item.description = product.description;
-        return true;
-      }
-    };
+  .factory('Product', function ($resource) {
+    return $resource('/api/products/:id', null, {
+      'update': { method: 'PUT'}
+    });
   });
+
