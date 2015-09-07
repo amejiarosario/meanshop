f46a4ffae124e4a4caa979fb8ac1e59b68df44ac
Adrian Mejia <adriansky@gmail.com>
2015-09-07 12:31:23 -0400

    remove Thing from the server side

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -19,7 +19,6 @@ function onConnect(socket) {
 
   // Insert sockets below
   require('../api/product/product.socket').register(socket);
-  require('../api/thing/thing.socket').register(socket);
 }
 
 module.exports = function(socketio) {
--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,6 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/products', require('./api/product'));
-  app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
