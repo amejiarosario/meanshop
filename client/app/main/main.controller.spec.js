a865dd1c942278632da27238ec47a4ae2eddb847
Adrian Mejia <adriansky@gmail.com>
2015-09-17 18:31:31 -0400

    fix main.controller unit tests

--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -4,21 +4,29 @@ describe('Controller: MainCtrl', function() {
 
   // load the controller's module
   beforeEach(module('meanshopApp'));
+  beforeEach(module('stateMock'));
+  beforeEach(module('socketMock'));
 
   var MainCtrl;
-  var $scope;
+  var scope;
   var state;
+  var $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function($controller, $rootScope, $state) {
-    $scope = $rootScope.$new();
+  beforeEach(inject(function(_$httpBackend_, $controller, $rootScope, $state) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('/api/products')
+      .respond(['Product 1', 'Product 2', 'Product 3', 'Product 4']);
+
+    scope = $rootScope.$new();
     state = $state;
     MainCtrl = $controller('MainCtrl', {
-      $scope: $scope
+      $scope: scope
     });
   }));
 
-  it('should attach a list of products to the scope', function() {
-    expect($scope.products.length).to.equal(2);
+  it('should attach a list of things to the scope', function() {
+    $httpBackend.flush();
+    expect(scope.products.length).to.equal(4);
   });
 });
