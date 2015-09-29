8b958f30ec10b7f0d95783f1f0cc1a94757e8bb1
Adrian Mejia <adriansky@gmail.com>
2015-09-29 10:58:09 -0400

    client product service tests

--- client/app/products/products.service.spec.js
+++ client/app/products/products.service.spec.js
@@ -8,13 +8,102 @@ describe('Service: Product', function () {
   beforeEach(module('meanshopApp'));
 
   // instantiate service
-  var Product;
-  beforeEach(inject(function (_Product_) {
+  var Product,
+      $httpBackend,
+      validAttributes = [
+        {title: 'Product1', price: 123.45 },
+        {title: 'Product2', price: 678.90 }
+      ],
+      newAttributes = {title: 'Product3', price: 1000 },
+      productWithId = angular.extend({}, newAttributes, {id: 123});
+
+  beforeEach(inject(function (_Product_, _$httpBackend_) {
     Product = _Product_;
+    $httpBackend = _$httpBackend_;
   }));
 
-  it('should do something', function () {
-    expect(!!Product).to.be.true;
+  describe('#index', function() {
+    it('should fetch products with HTTP GET request', function() {
+      $httpBackend.expectGET('/api/products').respond(validAttributes);
+      Product.query(function (products) {
+        expect(products).to.equal(validAttributes);
+      });
+    });
+
+    it('should work with empty data', function () {
+      $httpBackend.expectGET('/api/products').respond([]);
+      Product.query(function (products) {
+        expect(products).to.equal([]);
+      });
+    });
   });
 
+  describe('#show', function() {
+    it('should get a single product by id', function() {
+      $httpBackend
+        .expectGET('/api/products/1')
+        .respond(validAttributes[0]);
+      Product.get({id: 1}, function(product){
+        expect(product).to.equal(validAttributes[0]);
+      });
+    });
+  });
+
+  describe('#create', function() {
+    before(function() {
+      $httpBackend
+        .expect('POST', '/api/products', JSON.stringify(newAttributes))
+        .respond(productWithId);
+    });
+
+    it('should create a new Product from the class', function() {
+      var newProduct = Product.save(newAttributes, successCb(productWithId));
+      expect(to_json(newProduct)).to.eql(newAttributes);
+    });
+
+    it('should create a new product from the instance', function() {
+      var product = new Product();
+      product.title = 'Product3';
+      product.price = 1000;
+
+      product.$save(successCb(productWithId));
+      expect(to_json(product)).to.eql(newAttributes);
+    });
+
+  });
+
+  describe('#update', function() {
+    var updatedValues = {title: 'new title', price: 987};
+
+    it('should update attributes with PUT', function() {
+      $httpBackend
+        .expectPUT('/api/products/123', updatedValues)
+        .respond(angular.extend({}, updatedValues, {id: 123}));
+
+      Product.update({id: 123}, updatedValues, function(product){
+        expect(product.id).to.be(123);
+        expect(product.price).to.be(987);
+        expect(product.title).to.be('new title');
+      });
+    });
+  });
+
+  describe('#delete', function() {
+    it('should delete product', function() {
+      $httpBackend
+        .expectDELETE('/api/products/123')
+        .respond({});
+      Product.remove({id: 123}, successCb);
+    });
+  });
+
+  function to_json(obj){
+    return JSON.parse(JSON.stringify(obj));
+  }
+
+  function successCb(match){
+    return function(value/*, responseHeaders*/){
+      expect(value).to.equal(match);
+    };
+  }
 });
