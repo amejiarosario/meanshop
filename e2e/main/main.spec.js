3b0488c91fcf7af555fd1f0c539218dbb64b5626
Adrian Mejia <adriansky@gmail.com>
2015-09-30 07:39:49 -0400

    updates e2e tests

--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -11,8 +11,7 @@ describe('Main View', function() {
   });
 
   it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).to.eventually.equal('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).to.eventually.match(/yeoman.png$/);
-    expect(page.imgEl.getAttribute('alt')).to.eventually.equal('I\'m Yeoman');
+    expect(page.h1El.getText()).to.eventually.equal('MEANshop');
+    expect(page.imgEl.getAttribute('src')).to.eventually.match(/logo.png$/);
   });
 });
