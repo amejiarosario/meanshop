d2edd23fd132b9ec6bf124f2c6a4c4a43fc55394
Adrian Mejia <adriansky@gmail.com>
2015-10-06 11:16:05 -0400

    installing ngcart

--- client/app/app.js
+++ client/app/app.js
@@ -7,7 +7,8 @@ angular.module('meanshopApp', [
   'btford.socket-io',
   'ui.router',
   'ui.bootstrap',
-  'ngFileUpload'
+  'ngFileUpload',
+  'ngCart'
 ])
   .config(function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
