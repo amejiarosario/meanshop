8e2e2a1f4d2d8bdf980b2e5ce49c328b21036cf6
Adrian Mejia <adriansky@gmail.com>
2015-10-07 16:18:13 -0400

    enhancements to the order model

--- server/api/order/order.model.js
+++ server/api/order/order.model.js
@@ -3,15 +3,24 @@
 var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 var Schema = mongoose.Schema;
 
+var OrderDetailsSchema = new Schema({
+  product: { type: Schema.Types.ObjectId, ref: 'Product' },
+  quantity: Number,
+  total: Number
+});
+
 var OrderSchema = new Schema({
-  products: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+  total: { type: Number, required: true },
+  products: [OrderDetailsSchema],
+  name: String
   user: { type: Schema.Types.ObjectId, ref: 'User' },
   shipping: { type: Number, default: 0.0 },
   tax: { type: Number, default: 0.0 },
   discount: { type: Number, default: 0.0 },
-  total: { type: Number, required: true },
   status: { type: String, default: 'pending' }, // pending, in_progress, completed, canceled, refunded.
   payment: { type: String, default: 'paypal' },
+  shippingAddress: String,
+  billingAddress: String
 });
 
 module.exports = mongoose.model('Order', OrderSchema);
