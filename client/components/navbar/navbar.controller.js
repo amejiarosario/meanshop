f5a51576cb0b41c4327ad98cfa75eb9ad735066b
Adrian Mejia <adriansky@gmail.com>
2015-10-13 13:46:08 -0400

    Adding search to the navigation bar

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('meanshopApp')
-  .controller('NavbarCtrl', function ($scope, Auth, $rootScope, $state) {
+  .controller('NavbarCtrl', function ($scope, Auth, $rootScope, $state, $window, $timeout) {
     $scope.menu = [{
       'title': 'Home',
       'state': 'main'
@@ -17,5 +17,15 @@ angular.module('meanshopApp')
 
     $scope.search = function () {
       $rootScope.$broadcast('search:term', $scope.searchTerm);
-    }
+    };
+
+    $scope.redirect = function () {
+      $state.go('products');
+      // timeout makes sure that it is invoked after any other event has been triggered.
+      $timeout(function () {
+        // focus on search box
+        var searchBox = $window.document.getElementById('searchBox');
+        if(searchBox){ searchBox.focus(); }
+      })
+    };
   });
