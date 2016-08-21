902a6747503a1dd06a78e94c1b3747de7396bfa9
Adrian Mejia <admejiar@cisco.com>
2016-08-21 19:53:45 -0400

    static file web server

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
 const port = 9000;
 
 http.createServer(function (req, res) {
-  let body = [];
-
   console.log(`${req.method} ${req.url}`);
 
-  req
-    .on('data', function (data) {
-      body.push(data);
-    })
-    .on('end', function () {
-      console.log(`body: ${body.join()}`);
-    })
-    .on('error', function (err) {
-      console.error(err);
-      res.end(400, err);
-    });
+  const parsedUrl = url.parse(req.url);
+  const pathname = `.${parsedUrl.pathname}`;
+  const ext = path.parse(pathname).ext;
+  const map = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png'
+  };
 
-  res.end('bye!');
+  fs.readFile(pathname, function(err, data){
+    if(err){
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+    } else {
+      res.setHeader('Content-type', map[ext] || 'text/plain' );
+      res.end(data);
+    }
+  });
 
 }).listen(port);
 
