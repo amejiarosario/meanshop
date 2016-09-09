315545ce9ac7bed5e8ffd09edd29047aec6b654b
Adrian Mejia <admejiar@cisco.com>
2016-09-09 16:42:43 -0400

    Multiple products and editing selected

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,19 +1,38 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 import '../../public/stylesheets/styles.css';
 
 @Component({
-    selector: 'my-app',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Welcome to Meanshop';
-  product: Product = {
-    id: 1,
-    name: 'Building an e-Commerce app with MEAN',
-    price: 7
-  };
+
+  selectedProduct: Product;
+
+  onSelect(product: Product){
+    this.selectedProduct = product;
+  }
+
+  products: Product[] = [
+    {
+      id: 1,
+      name: 'Building an e-Commerce app with MEAN',
+      price: 8
+    },
+    {
+      id: 2,
+      name: 'Getting started with Angular 2 and ES6',
+      price: 7
+    },
+    {
+      id: 3,
+      name: 'Modern JavaScript',
+      price: 6
+    }
+  ];
 }
 
 export class Product {
