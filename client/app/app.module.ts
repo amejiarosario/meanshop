4eafa05de557e6fad3dac629e82ffa7b22417ea5
Adrian Mejia <admejiar@cisco.com>
2016-09-12 18:21:27 -0400

    adds http module

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
+import { HttpModule } from "@angular/http";
 import { FormsModule }   from '@angular/forms';
+import { BrowserModule }  from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { ProductsComponent, ProductDetailsComponent } from '../+products';
 import { routing } from "./app.routing";
+import { AppComponent } from './app.component';
 import { HomeComponent } from "../+home/home.component";
+import { ProductsComponent, ProductDetailsComponent } from '../+products';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
-    routing
+    routing,
+    HttpModule
   ],
   declarations: [
     AppComponent,
